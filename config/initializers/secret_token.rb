52a91809fb51003ca3b7b3e0f1c7e1d5326494bb
rufinomendoza <ThinkPadT400@Mobile>
2013-04-01 02:05:47 -0400

    Initial commit with home page work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Almawire::Application.config.secret_token = '285a2324ec710ec0ba99867b09b10673161e8227840dc82f243692fb1eb4d55944bbcb6c5aff8c8347efda30a294b0106fc5850f3bd6e6a58fee9549a558112a'
